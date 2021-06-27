d8058e009110b14a45d20e6f4226160a17021168
kaitoyamamoto931105 <kaitoyamamoto931105@outlook.com>
2021-06-27 18:34:33 +1000

    Commit20

---
+++ test/prod/utils/index.js
@@ -0,0 +1,32 @@
+const { detectNetworkName } = require('./detectNetwork');
+const { connectContract, connectContracts } = require('./connectContract');
+const {
+	ensureAccountHasEther,
+	ensureAccountHasSNX,
+	ensureAccountHassUSD,
+} = require('./ensureAccountHasBalance');
+const { exchangeSynths } = require('./exchangeSynths');
+const { readSetting, writeSetting } = require('./systemSettings');
+const { skipWaitingPeriod, skipStakeTime } = require('./skipWaiting');
+const { simulateExchangeRates } = require('./exchangeRates');
+const { takeDebtSnapshot } = require('./debtSnapshot');
+const { mockOptimismBridge } = require('./optimismBridge');
+const { implementsVirtualSynths } = require('./virtualSynths');
+
+module.exports = {
+	detectNetworkName,
+	connectContract,
+	connectContracts,
+	ensureAccountHasEther,
+	ensureAccountHassUSD,
+	ensureAccountHasSNX,
+	exchangeSynths,
+	readSetting,
+	writeSetting,
+	skipWaitingPeriod,
+	skipStakeTime,
+	simulateExchangeRates,
+	takeDebtSnapshot,
+	mockOptimismBridge,
+	implementsVirtualSynths,
+};
